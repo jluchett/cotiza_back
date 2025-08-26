252f19c4c54460c348b3eb193f7327b187b86e33
jluchett <jluchett@hotmail.com>
2025-08-26 10:28:04 -0500

    Agregado archivo .gitignore para excluir variables de entorno y node_modules

--- src/db.js
+++ src/db.js
@@ -5,8 +5,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+  port: process.env.DB_PORT
 });
 
 module.exports = {
